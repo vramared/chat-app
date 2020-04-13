2790e9c29f85857a8f2440967ea61edcbc313ab6
Vineet Ramareddi <vramared@ucsc.edu>
2020-04-13 13:05:46 -0700

    adding

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
 
+var port = process.env['PORT'] || 3000;
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
