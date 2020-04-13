5feb0596cc930738e4994299ee01715643dec5b2
Vineet Ramareddi <vramared@ucsc.edu>
2020-04-13 13:07:45 -0700

    test deployment

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
 
-var port = process.env['PORT'] || 3000;
+// var port = process.env['PORT'] || 3000;
+var port = 3000;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
