010d051eabcd9a8dd02e44ea8e138647dcebbd4e
Vineet Ramareddi <vramared@ucsc.edu>
2020-04-14 13:05:22 -0700

    formatting

--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ require('./db/mongoose.js');
 
 var port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/', function(req, res) {
+    res.send('Hello World!');
 });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,13 +4,13 @@ var user = process.env.MONGO_USER;
 var pw = process.env.MONGO_PW;
 const uri = `mongodb+srv://${user}:${pw}@cluster0-kiltc.mongodb.net/test?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, { 
+const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
